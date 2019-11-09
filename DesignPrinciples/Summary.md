9d466fdda33fcb23c2f17b4850dbfdcb85557032
Victor Delaplaine <vdelaplaines@aol.com>
2019-11-09 12:58:12 -0800

    autoadd

---
+++ DesignPrinciples/Summary.md
@@ -0,0 +1,21 @@
+# Summary for Design Principles
+
+## Single Responsible Principle
+	* A class should only have one reason to change
+	* Separation of concerns - diff classes handle diff, independent
+	task/problems
+## Open-Closed Principle
+	* Classes should be open for extension but closed for modification
+
+## Liskov-Substition Principle 
+	* You should be able to substitute a base type for a subtype
+
+## Interface-Segregation Principle
+	* Dont put to much into an interface; split into seperate interfaces
+	* YAGNI - you aint going to need it
+
+## Dependency Inversion Principle 
+	* High-Level modules should not depend upon low-level ones; use
+	abstractions(interfaces)
+
+
--- DesignPrinciples/src/com/company/ISP.java
+++ DesignPrinciples/src/com/company/ISP.java
@@ -1,4 +1,92 @@
 package com.company;
 
-public class ISP {
+// ISP - interface segregation principle
+// Seperating interfaces into smaller interfaces
+
+import javax.print.Doc;
+
+class Document{
+
 }
+
+interface Machine{
+    void print(Document d);
+    void fax(Document d);
+    void scan(Document d);
+}
+
+class MultiFunctionPrinter implements Machine{
+    @Override
+    public void print(Document d) {
+
+    }
+    @Override
+    public void fax(Document d) {
+    }
+    @Override
+    public void scan(Document d) {
+    }
+}
+class OldFashionedPrinter implements  Machine{
+    @Override
+    public void print(Document d) {
+    }
+    @Override
+    public void fax(Document d) {
+    }
+    @Override
+    public void scan(Document d) {
+    }
+}
+interface Printer{
+    void print(Document d);
+}
+interface Scanner{
+    void scan(Document d);
+}
+// YAGNI = you aint going to need it
+
+class JustAPrinter implements Printer{
+    @Override
+    public void print(Document d){
+
+    }
+}
+class Photocopier implements Printer, Scanner{
+
+    @Override
+    public void print(Document d){
+
+    }
+    @Override
+    public void scan(Document d){
+
+    }
+}
+
+interface MultiFunctionalDevice extends Printer, Scanner{}
+
+
+class MultiFunctionMachine implements MultiFunctionalDevice{
+
+    private Printer printer;
+    private Scanner scanner;
+
+    public MultiFunctionMachine(Printer printer, Scanner scanner){
+        this.printer = printer;
+        this.scanner = scanner;
+    }
+
+
+    @Override
+    public void print(Document d){
+        // Delegation or decarator design pattern
+        printer.print(d);
+    }
+
+    @Override
+    public void scan(Document d){
+        scanner.scan(d);
+    }
+}
+
